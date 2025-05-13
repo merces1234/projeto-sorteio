dc880947282c7fac62444dbf379fb1447cb2d893
merces1234 <vadinhomodesto9@gmail.com>
2025-05-13 20:09:50 -0300

    ATUALIZAÇAO DE VALORES

--- scripts.js
+++ scripts.js
@@ -3,7 +3,12 @@ function generateNumber() {
     const min = Math.ceil (document.querySelector(".input-min").value)
     const max = Math.floor (document.querySelector(".input-max").value)
 
-    const result = Math.floor(Math.random() * (max - min + 1)) + min;
+    if (min >= max) {
+        alert("o valor min tem que ser MENOR que o valor max")
+    } else { const result = Math.floor(Math.random() * (max - min + 1)) + min;
 
     alert(result)
+}
+
+   
 }
